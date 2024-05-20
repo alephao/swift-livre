4ceb266eacc9e07e915ff019c8bea2e61975df1b
alephao <7674479+alephao@users.noreply.github.com>
2024-05-19 21:19:57 -0300

    some dict fns

--- Sources/Livre/Dictonary+NonStandard.swift
+++ Sources/Livre/Dictonary+NonStandard.swift
@@ -2,18 +2,28 @@ public func toDictionary<Key: Hashable, Value>(_ keyValues: [(Key, Value)]) -> [
   return keyValues.reduce(into: [Key: Value](), { dict, kv in dict[kv.0] = kv.1 })
 }
 
-public func getting<Key: Hashable, Value>(key: Key) -> ([Key: Value]) -> Value? {
+public func value<Key: Hashable, Value>(for key: Key) -> ([Key: Value]) -> Value? {
   { dict in dict[key] }
 }
 
-public func getting<Key: Hashable, Value>(allKeys: [Key]) -> ([Key: Value]) -> [Value]? {
+public func values<Key: Hashable, Value>(for keys: [Key]) -> ([Key: Value]) -> [Value]? {
   { dict in
     var result: [Value] = []
-    for key in allKeys {
+    for key in keys {
       guard let value = dict[key] else {
         return nil
       }
       result.append(value)
+    }
+    return result
+  }
+}
+
+public func valuesOptional<Key: Hashable, Value>(for keys: [Key]) -> ([Key: Value]) -> [Value?] {
+  { dict in
+    var result: [Value?] = []
+    for key in keys {
+      result.append(dict[key])
     }
     return result
   }
